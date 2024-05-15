54b3e05e7e052611dad2652fdb67b20818b71bc6
Imran hassan <159555195+Imran-has@users.noreply.github.com>
2024-05-15 07:33:29 -0700

    Add files via upload

---
+++ Q5.ts
@@ -0,0 +1,41 @@
+import inquirer from"inquirer";
+let user= await inquirer . prompt ([{
+  name: "firstNumber",
+  type:"number",
+  message:"enter your firstnumber number"
+  
+  
+},
+
+{
+
+name: "secondNumber",
+  type:"number",
+  message:"enter your second number"
+  
+},
+
+{
+
+name: "operator",
+  type:"list",
+  message:"select one of the operator to perform the operation ",
+  choices:["Addition","Substration"]
+  
+}]);
+let {firstNumber, secondNumber, operator}=user;
+function num(num1:number,num2:number,operation: string){
+ switch (operation){
+  case "Addition":
+ return `The sum of ${num1} && ${num2} is ${num1+num2}`;
+ break;
+ case "Substration":
+ return `The sum of ${num1} && ${num2} is ${num1-num2}`;
+ break;
+ default:{
+console.log ("please select right number");
+}
+}
+}
+let response =num(firstNumber, secondNumber, operator);
+console.log(response);
