a6e956a2bb7768f2e556158eb2df9097d2efc1f5
Imran hassan <159555195+Imran-has@users.noreply.github.com>
2024-05-15 07:31:57 -0700

    Add files via upload

---
+++ Q4.js
@@ -0,0 +1,9 @@
+"use strict";
+//task# 4 
+function multiple(a, b) {
+    return a * b;
+}
+let num1 = 5;
+let num2 = 10;
+console.log("Result of first cal:", multiple(num1, num2));
+console.log("Result of second call:", multiple(4, 10));
---
+++ Q4.ts
@@ -0,0 +1,10 @@
+//task# 4 
+     function multiple (a:number ,b:number){
+       return  a*b;
+     }
+     
+     let num1 =5;
+     let num2=10;
+    console.log ("Result of first cal:", multiple(num1, num2) );
+    
+    console.log ("Result of second call:", multiple(4, 10) );
