f64a5fae0ce0b120146aa99e9cb8d950202ff0bb
Imran hassan <159555195+Imran-has@users.noreply.github.com>
2024-05-15 07:30:06 -0700

    Add files via upload

---
+++ Q1.js
@@ -0,0 +1,5 @@
+let miles = 10;
+let convert_into_km = miles * 1.60934;
+let kilometers = convert_into_km;
+console.log(`The distance of ${kilometers}kilometers is equal to  ${miles} miles`);
+export {};
---
+++ Q1.ts
@@ -0,0 +1,6 @@
+let miles:number = 10;  
+
+let convert_into_km:number = miles * 1.60934;
+let kilometers=convert_into_km
+
+console.log(`The distance of ${kilometers}kilometers is equal to  ${miles} miles`);
