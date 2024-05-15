35ec8ac201cf9d7b5b84a7cc1fad8ec1b3f144e2
Imran hassan <159555195+Imran-has@users.noreply.github.com>
2024-05-15 07:31:26 -0700

    Add files via upload

---
+++ Q3.ts
@@ -0,0 +1,22 @@
+//task #3
+     import inquirer from"inquirer";
+     
+     let user: {name:string}=await inquirer.prompt([{
+         name: "name",
+         type: "input",
+         message:"enter your name"
+     }])
+     let {name}= user;
+     let userName=name.toLowerCase()
+     switch (userName){
+         case "imran": console.log("this is your known friend");
+         break;
+         case "saad": console.log("this is your known friend");
+         break;
+         case "Abdullah": console.log("this is your known friend");
+         break;
+         case "tariq": console.log("this is your known friend");
+         break;
+         default: console.log( "deault response");
+         
+     };
