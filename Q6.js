5ecf60735051b862c5e468d780b8cc54ee2a187f
Imran hassan <159555195+Imran-has@users.noreply.github.com>
2024-05-15 07:34:19 -0700

    Add files via upload

---
+++ Q6.js
@@ -0,0 +1,9 @@
+//task#6
+// anonymous function
+//function expression assigned to a veriable
+let func1 = function (name) {
+    console.log(name);
+};
+//passing an argument into the function
+func1("Hello world");
+export {};
