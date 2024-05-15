03c9f8ff6039627a5694e97b8f355a40306740e0
Imran hassan <159555195+Imran-has@users.noreply.github.com>
2024-05-15 07:30:47 -0700

    Add files via upload

---
+++ Q2.ts
@@ -0,0 +1,26 @@
+//Task #2
+import inquirer from "inquirer";
+let dynamicNumber= 10;//Math.floor(Math.random() *10+1)
+//Prompt the user to  enter  enter a number 
+
+let userNumber = await inquirer.prompt ([{
+     name :"game" ,
+     type: "number",
+     message:"Enter a your number"
+    
+}])
+let name=userNumber.game;
+ 
+ if (name>dynamicNumber){
+ console.log(`your number is greater than the dynamicNumber value`);
+
+ }
+ else if(name<dynamicNumber){
+ console.log("your number is less than the daynmicNumber");
+     
+ }
+  
+ else
+  {
+    console.log("your number is equal to the Dynamic value " );
+ } 
